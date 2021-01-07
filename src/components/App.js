82c71d3fb704bf18f90cce05811c32340a35ffcc
Andreas Bigger <abigger87@gmail.com>
2021-01-07 09:01:13 -0800

    finished start code

--- src/components/App.js
+++ src/components/App.js
@@ -17,11 +17,11 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    if (window.ethereum) {
+    if (window && window.ethereum) {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
     }
-    else if (window.web3) {
+    else if (window && window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
     }
     else {
@@ -31,62 +31,99 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3
-    //Load accounts
-    //Add first account the the state
+   
+    // * Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    
+    // * Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DVideo.networks[networkId]
 
-    //Get network ID
-    //Get network data
-    //Check if net data exists, then
-      //Assign dvideo contract to a variable
-      //Add dvideo to the state
+     // * Only continure if network data
+    if(networkData) {
+      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address)
+      this.setState({ dvideo })
 
-      //Check videoAmounts
-      //Add videAmounts to the state
+      // * Check videoAmounts and add to state
+      const videosCount = await dvideo.methods.videoCount().call();
+      this.setState({ videosCount })
 
-      //Iterate throught videos and add them to the state (by newest)
+      // * Iterate and fetch all videos
+      for(let i = videosCount; i >= 1; i--) {
+        const video = await dvideo.methods.videos(i).call();
+        this.setState({ videos: [...this.state.videos, video]});
+      }
 
+      // * Set latest video as default
+      const latest = await dvideo.methods.videos(videosCount).call();
+      this.setState({ currentHash: latest.hash, currentTitle: latest.title });
 
-      //Set latest video and it's title to view as default 
-      //Set loading state to false
-
-      //If network data doesn't exisits, log error
+      // * Finished loading
+      this.setState({ loading: false})
+    } else {
+      window.alert('DVideo contract not deployed to detected network.')
+    }
   }
 
-  //Get video
-  captureFile = event => {
+  captureFile = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
 
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+    }
   }
 
-  //Upload video
-  uploadVideo = title => {
+  uploadVideo = (title) => {
+    ipfs.add(this.state.buffer, (error, result) => {
+      if(error) {
+        console.error(error)
+        return
+      }
 
+      this.setState({ loading: true })
+      this.state.dvideo.methods.uploadVideo(result[0].hash, title).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
   }
 
   //Change Video
   changeVideo = (hash, title) => {
-
+    this.setState({'currentHash': hash});
+    this.setState({'currentTitle': title});
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      loading: false
-      //set states
+      loading: true,
+      account: '',
+      dvideo: null,
+      videos: [],
+      buffer: null,
+      currentHash: null,
+      currentTitle: null
     }
-
-    //Bind functions
+    this.uploadVideo = this.uploadVideo.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+    this.changeVideo = this.changeVideo.bind(this)
   }
 
   render() {
     return (
       <div>
-        <Navbar 
-          //Account
-        />
+        <Navbar account={this.state.account} />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-              //states&functions
+              videos={this.state.videos}
+              captureFile={this.captureFile}
+              uploadVideo={this.uploadVideo}
+              changeVideo={this.changeVideo}
             />
         }
       </div>
--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract DVideo {
-  uint public videoCount = 0;
-  string public name = "DVideo";
-  //Create id=>struct mapping
+    uint256 public videoCount = 0;
+    string public name = "DVideo";
+    mapping(uint256 => Video) public videos;
 
-  //Create Struct
+    struct Video {
+        uint256 id;
+        string hash;
+        string title;
+        address author;
+    }
 
+    event VideoStored(uint256 id, string hash, string title, address author);
 
-  //Create Event
+    constructor() public {}
 
+    function uploadVideo(string calldata _videoHash, string calldata _title)
+        external
+    {
+        // Make sure the video hash exists
+        require(bytes(_videoHash).length > 0, "Video Hash must not be empty.");
 
-  constructor() public {
-  }
+        // Make sure video title exists
+        require(bytes(_title).length > 0, "Video Title must not be empty.");
 
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
+        // Make sure uploader address exists
+        require(msg.sender != address(0x0), "Video Hash must not be empty.");
 
-    // Make sure video title exists
+        // Increment video id
+        ++videoCount;
 
-    // Make sure uploader address exists
+        // Add video to the contract
+        videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
 
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
-  }
+        // Trigger an event
+        emit VideoStored(videoCount, _videoHash, _title, msg.sender);
+    }
 }
